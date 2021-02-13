9ef1f0af660b5f19c2384e9fdd8452f3aab06bc1
MrAhmedEgeh <mrxb114@gmail.com>
2021-02-13 20:36:32 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function openSideMenu(){
+    document.getElementById('side-list').style.width = '250px';
+}
+function closeSideMenu(){
+    document.getElementById('side-list').style.width = '0px';
+}
